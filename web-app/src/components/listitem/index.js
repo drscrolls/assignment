7bcebb8c04dede439e5030cc23f76abc113b981e
Andrew <andrew@fynderr.shop>
2023-05-03 14:21:37 +0000

    getting data from api

--- web-app/src/components/listitem/index.js
+++ web-app/src/components/listitem/index.js
@@ -1,28 +1,39 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenNib, faPhone, faTrash, faTrashCan } from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
 
-export default function ListItem() {
-  const [showModal, setShowModal] = useState(false);
-  const handleDelete = () => {
+const formatPhoneNumber = (phoneNumberString) => {
+  phoneNumberString= phoneNumberString.replace("+", "");
 
+  const cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  if (!match) {
+    return phoneNumberString;
   }
+  return match[1] + '-' + match[2] + '-' + match[3];
+}
 
+export default function ListItem({contact, handleDelete}) {
+  let {_id, firstname, lastname, phonenumber } = contact;
+  const [showModal, setShowModal] = useState(false);
+  phonenumber = formatPhoneNumber(phonenumber);
+
+    
+
+  
   return (
     <div className="p-3 border-bottom" style={{ display: "flex", flex: 1 }}>
       <div style={{ justifyContents: "start", flex: 1 }}>
-        <h5>Contacts</h5>
+        <h5>{firstname} {lastname}</h5>
         <h6 className="text-muted">
           <FontAwesomeIcon icon={faPhone} style={{ marginRight: 5, width: 13 }} />
-          233 453 4839</h6>
+          {phonenumber}</h6>
       </div>
 
       <div style={{ justifyContents: "center" }}>
-        <Button variant="danger" color="#e0373f" onClick={handleDelete}>
+        <Button variant="danger" color="#e0373f" onClick={()=> handleDelete(_id)}>
           <FontAwesomeIcon icon={faTrashCan} />
         </Button>
       </div>
--- web-app/src/pages/Home/index.js
+++ web-app/src/pages/Home/index.js
@@ -1,32 +1,127 @@
-import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import FormGroup from 'react-bootstrap/esm/FormGroup';
 import InputGroup from 'react-bootstrap/InputGroup';
-import FormLabel from 'react-bootstrap/esm/FormLabel';
 import FormControl from 'react-bootstrap/esm/FormControl';
-import FormText from 'react-bootstrap/esm/FormText';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAdd, faAddressBook } from '@fortawesome/free-solid-svg-icons';
 import Card from 'react-bootstrap/Card';
 import ListItem from '../../components/listitem';
+import axios from 'axios';
 import "./style.css";
-import { faAdd, faAddressBook } from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Home() {
+  const [data, setData] = useState([]);
+  const [fetchingData, setFetchingData] = useState(true);
+
   const [showModal, setShowModal] = useState(false);
   const handleClose = () => setShowModal(false);
   const handleShow = () => setShowModal(true);
 
 
-  const AddContactModal = () => {
-    
-    const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    getContacts();
+  }, [])
 
-    const handleSubmit = () => {
-      setLoading(true);
-    }
+
+  const deleteContact = async (id) => {
+    setFetchingData(true);
+    await axios.delete(`${process.env.React_App_API_BASEURL}/contacts/${id}`)
+      .then(res => {
+        setFetchingData(false);
+        if (res.status === 200) {
+          setData(res.data);
+        } else {
+          // setMessage(res.data.message);
+          // setSubmittedForm(true);
+        }
+      })
+      .catch(error => {
+        console.log("error", error);
+        // setMessage(error);
+        setFetchingData(false);
+      });
+  }
+
+  
+  const updateContact = async (id, payload) => {
+    setFetchingData(true);
+    await axios.put(`${process.env.React_App_API_BASEURL}/contacts/${id}`, payload)
+      .then(res => {
+        setFetchingData(false);
+        if (res.status === 200) {
+          setData(res.data);
+        } else {
+          // setMessage(res.data.message);
+          // setSubmittedForm(true);
+        }
+      })
+      .catch(error => {
+        console.log("error", error);
+        // setMessage(error);
+        setFetchingData(false);
+      });
+  }
+
+
+  const getContacts = async () => {
+    setFetchingData(true);
+    let baseUrl = process.env.React_App_API_BASEURL;
+    console.log({baseUrl});
+
+    await axios.get(`${baseUrl}/contacts`)
+      .then(res => {
+        setFetchingData(false);
+        if (res.status === 200) {
+          setData(res.data.data);
+        } else {
+          // setMessage(res.data.message);
+          // setSubmittedForm(true);
+        }
+      })
+      .catch(error => {
+        console.log("error", error);
+        // setMessage(error);
+        setFetchingData(false);
+      });
+  }
+
+
+
+  const AddContactModal = () => {
+    const [isLoading, setLoading] = useState(false);
+    const [firstname, setFirstname] = useState(null);
+    const [lastname, setLastname] = useState(null);
+    const [phonenumber, setPhonenumber] = useState(null);
+
+    
+  const addContact = async () => {
+    setFetchingData(true);
+
+    let payload = {
+      firstname,
+      lastname,
+      phonenumber
+    };
+
+    await axios.post(`${process.env.React_App_API_BASEURL}/contacts`, payload)
+      .then(res => {
+        setFetchingData(false);
+        if (res.status === 200) {
+          setData(res.data);
+        } else {
+          // setMessage(res.data.message);
+          // setSubmittedForm(true);
+        }
+      })
+      .catch(error => {
+        console.log("error", error);
+        // setMessage(error);
+        setFetchingData(false);
+      });
+  }
+
 
     return <>
       <Modal show={showModal} onHide={handleClose}>
@@ -42,8 +137,7 @@ export default function Home() {
           <Button
             variant="primary"
             disabled={isLoading}
-            onClick={!isLoading ? handleSubmit : null}
-          >
+            onClick={!isLoading ? addContact : null}>
             {isLoading ? 'Loading…' : 'Submit'}
           </Button>
         </Modal.Footer>
@@ -55,33 +149,35 @@ export default function Home() {
     <div className="container mt-5 d-block">
 
       <h2 align="center"><FontAwesomeIcon icon={faAddressBook} />  Phone Book App</h2>
-      <div className="p-2" style={{display: "flex", flex: 1}}>
-        <h4 style={{justifyContents: "start", flex: 1}}>Contacts</h4>
-        <div style={{justifyContents: "end"}}>
+      <div className="p-2" style={{ display: "flex", flex: 1 }}>
+        <h4 style={{ justifyContents: "start", flex: 1 }}>Contacts</h4>
+        <div style={{ justifyContents: "end" }}>
           <Button variant="primary" onClick={handleShow}>
-              <FontAwesomeIcon icon={faAdd} style={{marginRight: 5}} />
-              Add Contact
-            </Button>
+            <FontAwesomeIcon icon={faAdd} style={{ marginRight: 5 }} />
+            Add Contact
+          </Button>
         </div>
       </div>
 
-      
+
       <div className="py-2">
         <InputGroup className="mb-3">
-          <FormControl aria-label="Search..." placeholder="Search for contact by lastname..."/>
+          <FormControl aria-label="Search..." placeholder="Search for contact by lastname..." />
         </InputGroup>
       </div>
 
       <AddContactModal />
-      
-      <Card style={{width: "100%"}}>
+
+      <Card style={{ width: "100%" }}>
         <Card.Body className="p-0">
-              
-        <ListItem />
-        <ListItem />
-        <ListItem />
 
-
+          {fetchingData ? <p align="center" className="text-muted p-2 mb-0">Loading...</p> :
+            data && data.length > 0 ?
+            data.map((contact, index) => (
+              <ListItem key={index} contact={contact} handleDelete={deleteContact} />
+            )) :
+            <p align="center" className="text-muted p-2 mb-0">No data found.</p>
+          }
         </Card.Body>
       </Card>
 
